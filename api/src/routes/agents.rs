774c17e33779cb36a080f989c5ac4f981ce15dbd
roverchen <rover.k.chen@gmail.com>
2020-03-24 16:15:26 +0800

    handler::SmartState::get_agent(&self, &public_key);

--- api/src/routes/agents.rs
+++ api/src/routes/agents.rs
@@ -181,9 +181,10 @@ pub fn get_agent(
     public_key: String
 ) -> Result<JsonValue, Errors> {
 //) -> Result<Option<Agent>, ApplyError> {
-    let context = TransactionContext;
-    let state = handler::SmartState::new(context);
-    handler::get_agent_by_public_key(&public_key, &state);
+    //let context = TransactionContext;
+    //let state = handler::SmartState::new(context);
+    //handler::get_agent_by_public_key(&public_key, &state);
+    handler::SmartState::get_agent(&self, &public_key);
     Ok(json!({ "getAgent": "done" }))
 }
 //pub fn get_agent(auth: Auth, conn: db::Conn, state: State<AppState>) -> Option<JsonValue> {
