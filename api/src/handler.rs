91c48e6da20d101fe3d2f64b5a164f2de4eb1ccf
roverchen <rover.k.chen@gmail.com>
2020-03-25 09:53:42 +0800

    update

--- api/src/handler.rs
+++ api/src/handler.rs
@@ -285,7 +285,9 @@ pub fn get_agent_by_public_key(
     }
 
     // make sure agent already exists
-    let context = SmartTransactionHandler;
+    let context = signing::create_context("secp256k1")
+        .expect("Error creating the right context");
+    //let context = SmartTransactionHandler;
     let state = SmartState.new(context);
     let mut agent = match state.get_agent(public_key) {
         Ok(None) => {
