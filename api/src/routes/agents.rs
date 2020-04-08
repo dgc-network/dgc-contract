4c1c538fc6670870d1cdb5733d4029b628268dd8
roverchen <rover.k.chen@gmail.com>
2020-04-08 21:27:21 +0800

    submit_batch_list(         &format!("{}/batches?wait=120", url),         &batch_list);

--- api/src/routes/agents.rs
+++ api/src/routes/agents.rs
@@ -12,9 +12,9 @@ use serde::Deserialize;
 use validator::Validate;
 //use submit::do_create;
 use payload::{
-    create_agent_payload
+    create_agent_payload,
+    update_agent_payload
 //    create_org_payload,
-//    update_agent_payload,
 //    update_org_payload
 };
 //use protos::state::KeyValueEntry;
@@ -40,6 +40,10 @@ use transaction::{
 //use submit::submit_batch_list;
 use addresser::{compute_agent_address};
 
+use hyper::Client;
+use hyper::body::HttpBody as _;
+use tokio::io::{stdout, AsyncWriteExt as _};
+
 //use rocket::Outcome;
 //use rocket::http::Status;
 //use rocket::request::{self, Request, FromRequest};
@@ -53,7 +57,7 @@ pub struct NewAgent {
 
 #[derive(Deserialize, Validate)]
 struct NewAgentData {
-    private_key: Option<String>,
+    //private_key: Option<String>,
     org_id: Option<String>, 
     roles: Option<String>, 
     metadata: Option<String>
@@ -79,20 +83,16 @@ pub fn post_agents(
     let org_id = extractor.extract("org_id", new_agent.org_id);
     let roles_as_strings = extractor.extract("roles", new_agent.roles);
     let metadata_as_strings = extractor.extract("metadata", new_agent.metadata);
-    let private_key_hex_string = extractor.extract("private_key", new_agent.private_key);
+    //let private_key_hex_string = extractor.extract("private_key", new_agent.private_key);
 
     extractor.check()?;
 
-    //let url = "http://dgc-api:9001";
     let context = signing::create_context("secp256k1")
         .expect("Error creating the right context");
-    //let private_key = context.new_random_private_key()
-    //    .expect("Error generating a new Private Key");
-    let private_key = Secp256k1PrivateKey::from_hex(&private_key_hex_string)
-        .expect("Error retrieving Private Key");
-    let crypto_factory = signing::CryptoFactory::new(context.as_ref());
-    //let signer = crypto_factory.new_signer(&private_key.as_ref());
-    let signer = crypto_factory.new_signer(&private_key);
+    let private_key = context.new_random_private_key()
+        .expect("Error generating a new Private Key");
+    let factory = signing::CryptoFactory::new(context.as_ref());
+    let signer = factory.new_signer(&private_key);
     let public_key = signer.get_public_key()
         .expect("Error retrieving Public Key")
         .as_hex();
@@ -124,6 +124,7 @@ pub fn post_agents(
             None => "Metadata is formated incorrectly".to_string()
         };
         let mut entry = KeyValueEntry::new();
+
         entry.set_key(key);
         entry.set_value(value);
         metadata.push(entry.clone());
@@ -167,20 +168,111 @@ pub fn post_agents(
 */
 }
 
+//#[derive(Deserialize)]
+//pub struct UpdateAgent {
+//    agent: db::users::UpdateUserData,
+//}
 #[derive(Deserialize)]
 pub struct UpdateAgent {
-    agent: db::users::UpdateUserData,
+    agent: UpdateAgentData,
+}
+
+#[derive(Deserialize, Validate)]
+struct UpdateAgentData {
+    private_key: Option<String>,
+    org_id: Option<String>, 
+    roles: Option<String>, 
+    metadata: Option<String>
+/*
+    #[validate(length(min = 1))]
+    username: Option<String>,
+    #[validate(email)]
+    email: Option<String>,
+    #[validate(length(min = 8))]
+    password: Option<String>,
+*/
 }
 
 #[put("/agent", format = "json", data = "<agent>")]
 pub fn put_agent(
-    agent: Json<UpdateAgent>,
-    auth: Auth,
-    conn: db::Conn,
-    state: State<AppState>,
+    update_agent: Json<UpdateAgent>,
+    //agent: Json<UpdateAgent>,
+    //auth: Auth,
+    //conn: db::Conn,
+    //state: State<AppState>,
 ) -> Option<JsonValue> {
-    db::users::update(&conn, auth.id, &agent.agent)
-        .map(|user| json!({ "user": user.to_user_auth(&state.secret) }))
+    //db::users::update(&conn, auth.id, &agent.agent)
+    //    .map(|user| json!({ "user": user.to_user_auth(&state.secret) }))
+
+    let update_agent = update_agent.into_inner().agent;
+
+    let mut extractor = FieldValidator::validate(&update_agent);
+    let org_id = extractor.extract("org_id", update_agent.org_id);
+    let roles_as_strings = extractor.extract("roles", update_agent.roles);
+    let metadata_as_strings = extractor.extract("metadata", update_agent.metadata);
+    let private_key_hex_string = extractor.extract("private_key", update_agent.private_key);
+
+    extractor.check()?;
+
+    let context = signing::create_context("secp256k1")
+        .expect("Error creating the right context");
+    let private_key = Secp256k1PrivateKey::from_hex(&private_key_hex_string)
+        .expect("Error retrieving Private Key");
+    let crypto_factory = signing::CryptoFactory::new(context.as_ref());
+    let signer = crypto_factory.new_signer(&private_key);
+    let public_key = signer.get_public_key()
+        .expect("Error retrieving Public Key")
+        .as_hex();
+
+    let mut roles = Vec::<String>::new();
+    for role in roles_as_strings.chars() {
+        let entry: String = role.to_string().split(",").collect();
+        roles.push(entry.clone());
+    }
+
+    let mut metadata = Vec::<KeyValueEntry>::new();
+    for meta in metadata_as_strings.chars() {
+        let meta_as_string = meta.to_string();
+        let key_val: Vec<&str> = meta_as_string.split(",").collect();
+        if key_val.len() != 2 {
+            "Metadata is formated incorrectly".to_string();            
+        }
+        let key = match key_val.get(0) {
+            Some(key) => key.to_string(),
+            None => "Metadata is formated incorrectly".to_string()
+        };
+        let value = match key_val.get(1) {
+            Some(value) => value.to_string(),
+            None => "Metadata is formated incorrectly".to_string()
+        };
+        let mut entry = KeyValueEntry::new();
+
+        entry.set_key(key);
+        entry.set_value(value);
+        metadata.push(entry.clone());
+    }
+
+    let payload = update_agent_payload(&org_id, &public_key, roles, metadata);    
+
+    let addresses = protobuf::RepeatedField::from_vec(vec![
+        compute_agent_address(&org_id),
+        compute_agent_address(&public_key),
+        compute_agent_address(&public_key),
+    ]);
+
+    let txn = create_transaction(addresses, &payload, &signer, &public_key)
+        .expect("Error create transaction");
+    let batch = create_batch(txn, &signer, &public_key)
+        .expect("Error create batch");
+    let batch_list = create_batch_list_from_one(batch);
+
+    let url = "http://dgc-api:9001";
+    submit_batch_list(
+        &format!("{}/batches?wait=120", url),
+        &batch_list);
+
+    Ok(json!({ "updateAgent": "done" }))
+
 }
 
 #[derive(Deserialize)]
@@ -212,67 +304,28 @@ pub fn post_agents_login(
         .ok_or_else(|| Errors::new(&[("email or password", "is invalid")]))
 }
 
-/*
-#[derive(Deserialize)]
-pub struct AgentContext {
-    context: &mut dyn TransactionContext,
-}
-
-impl<'a, 'r> FromRequest<'a, 'r> for AgentContext {
-    type Error = ();
-    //pub fn new(context: &'a mut dyn TransactionContext) -> AgentContext {
-    //    AgentContext { context: context }
-    //}
-
-    fn from_request(request: &'a Request<'r>) -> request::Outcome<Self, Self::Error> {
-        AgentContext { context: context }
-        //AgentContext { context: Self }
-        //let keys: Vec<_> = request.headers().get("x-api-key").collect();
-        //match keys.len() {
-        //    0 => Outcome::Failure((Status::BadRequest, ApiKeyError::Missing)),
-        //    1 if is_valid(keys[0]) => Outcome::Success(ApiKey(keys[0].to_string())),
-        //    1 => Outcome::Failure((Status::BadRequest, ApiKeyError::Invalid)),
-        //    _ => Outcome::Failure((Status::BadRequest, ApiKeyError::BadCount)),
-        //}
-    }
-}
-*/
-
 #[get("/agent/<public_key>")]
 pub fn get_agent(
-    //&mut self, 
     public_key: String,
-    //context: &mut dyn TransactionContext,
-    //context: AgentContext,
 ) -> Result<JsonValue, Errors> {
-//) -> Result<Option<Agent>, ApplyError> {
-    //let context = TransactionContext::new();
-    //let state = handler::SmartState::new(context);
-    //handler::get_agent_by_public_key(&public_key, &state);
-    //handler::SmartState::get_agent(&self, &public_key);
-    //handler::get_agent_by_public_key(&public_key, context);
-    //GetAgent::by_public_key(&public_key);
-/*
-    //let state = handler::SmartState::new(self.context);
-    let state = handler::SmartState::new(context);
-    let mut agent = match state.get_agent(&public_key) {
-        Ok(None) => {
-            //return Err(ApplyError::InvalidTransaction(format!(
-            //    "Agent does not exists: {}",
-            //    public_key,
-            //)))
-            return Err(json!({ "Agent does not exists": public_key }))
-        }
-        Ok(Some(agent)) => agent,
-        Err(err) => {
-            //return Err(ApplyError::InvalidTransaction(format!(
-            //    "Failed to retrieve state: {}",
-            //    err,
-            //)))
-            return Err(json!({ "Failed to retrieve state": err }))
-        }
-    };
+
+    // Previously...
+    let client = Client::new();
+/*    
+    let url = "http://dgc-api:9001";
+    submit_batch_list(
+        &format!("{}/batches?wait=120", url),
+        &batch_list);
 */
+    let uri = "http://httpbin.org/ip".parse()?;
+    let mut resp = client.get(uri).await?;
+    println!("Response: {}", resp.status());
+
+    // And now...
+    while let Some(chunk) = resp.body_mut().data().await {
+        stdout().write_all(&chunk?).await?;
+    }
+
     Ok(json!({ "getAgent": "done" }))
 }
 //pub fn get_agent(auth: Auth, conn: db::Conn, state: State<AppState>) -> Option<JsonValue> {
