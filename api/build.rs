cfa10e248a556fefae885fd1759acd6e4b41e8ed
roverchen <rover.k.chen@gmail.com>
2020-04-14 22:11:35 +0800

    extern crate protobuf_codegen_pure;

--- api/build.rs
+++ api/build.rs
@@ -1,6 +1,7 @@
 // Copyright (c) The dgc.network
 // SPDX-License-Identifier: Apache-2.0
 
+extern crate protobuf_codegen_pure;
 extern crate protoc_rust;
 use protoc_rust::Customize;
 
@@ -10,14 +11,26 @@ use std::io::prelude::*;
 
 fn main() {
     fs::create_dir_all("src/protos").unwrap();
+/*
     protoc_rust::run(protoc_rust::Args {
         out_dir: "src/protos",
         input: &["../protos/payload.proto", "../protos/state.proto"],
         includes: &["../protos"],
         customize: Customize::default(),
     }).expect("protoc");
-
+*/
+    protobuf_codegen_pure::run(protobuf_codegen_pure::Args {
+        out_dir: "src/protos",
+        input: &["../protos/payload.proto", "../protos/state.proto"],
+        includes: &["../protos"],
+        customize: Customize {
+          ..Default::default()
+        },
+    }).expect("protoc");
+    
     let mut file = File::create("src/protos/mod.rs").unwrap();
     file.write_all(b"pub mod payload;\n").unwrap();
     file.write_all(b"pub mod state;\n").unwrap();
 }
+
+
