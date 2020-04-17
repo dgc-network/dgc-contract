4240d02ad85433afde3e0be35468f70a27c3051d
roverchen <rover.k.chen@gmail.com>
2020-04-17 12:55:35 +0800

    Customize::default()

--- api/build.rs
+++ api/build.rs
@@ -1,9 +1,8 @@
 // Copyright (c) The dgc.network
 // SPDX-License-Identifier: Apache-2.0
 
-extern crate protobuf_codegen_pure;
 extern crate protoc_rust;
-use protoc_rust::Customize;
+use protoc_rust::{Customize, Codegen};
 
 use std::fs;
 use std::fs::File;
@@ -11,6 +10,16 @@ use std::io::prelude::*;
 
 fn main() {
     fs::create_dir_all("src/protos").unwrap();
+
+    Codegen::new()
+    .protoc_path(protoc_bin_vendored::protoc_bin_path().unwrap())
+    .out_dir("src/protos")
+    .input(&["../protos/payload.proto", "../protos/state.proto"])
+    .includes(&["../protos"])
+    .customize(Customize::default())
+    // ...
+    .run()
+    .unwrap();
 /*
     protoc_rust::run(protoc_rust::Args {
         out_dir: "src/protos",
@@ -18,16 +27,7 @@ fn main() {
         includes: &["../protos"],
         customize: Customize::default(),
     }).expect("protoc");
-*/
-    protobuf_codegen_pure::run(protobuf_codegen_pure::Args {
-        out_dir: "src/protos",
-        input: &["../protos/payload.proto", "../protos/state.proto"],
-        includes: &["../protos"],
-        customize: Customize {
-          ..Default::default()
-        },
-    }).expect("protoc");
-    
+*/    
     let mut file = File::create("src/protos/mod.rs").unwrap();
     file.write_all(b"pub mod payload;\n").unwrap();
     file.write_all(b"pub mod state;\n").unwrap();
